7477dc463c01f3906734b64872bdb65f27889d38
Lian Aarts <lian.aarts@u-bordeaux.fr>
2022-04-12 15:33:06 +0200

    removed secret.h from gitignore

---
+++ painless_MQTT/src/secret.h
@@ -0,0 +1,5 @@
+#define   MESH_PREFIX_pass     "MeshNetwork_UIT"
+#define   MESH_PASSWORD_pass   "MeshPassword_UIT"
+
+#define   STATION_SSID_pass     "MeshNetwork"
+#define   STATION_PASSWORD_pass "MeshNetwork2022_esp"
---
+++ rootselection/src/secret.h
@@ -0,0 +1,5 @@
+#define MESH_PREFIX_secr    "LiansNetwork"
+#define MESH_PASSWORD_secr  "LiansNetwork"
+#define token_secr          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI4YTMyYWI2ZDMzNGE0MjIzODM5MzIzYzQ4MTE5YzE2OCIsImlhdCI6MTY0OTE2Mjc1NiwiZXhwIjoxOTY0NTIyNzU2fQ.AXA7uUs4cRBxNI0JDZGX1U9fqrxuLeWr_XiN5_jRndk"
+#define ssidScan_secr       "home-assistant-AP"
+#define passScan_secr       "networkubdx"
